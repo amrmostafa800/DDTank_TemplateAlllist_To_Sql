a2f5bcfef677b5c603056580ed87f6afdeedb262
amrmostafa800 <amrmostafa800@gmail.com>
2022-04-27 03:06:58 +0200

    fix NULL Bug

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Xml;
 using System.Threading;
-
-
-
+using System.Threading.Tasks;
 
 var path = Path.Combine(Directory.GetCurrentDirectory(), "templatealllist_decoded.xml");
 var path2 = Path.Combine(Directory.GetCurrentDirectory(), "TemplateAlllist.sql");
@@ -19,18 +17,25 @@ streamWriter.WriteLine("GO");
 streamWriter.WriteLine("UPDATE dbo.Shop_Goods");
 while (xmlReader.Read())
     {
-  if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "Item" && xmlReader.HasAttributes)
+  if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "Item" && xmlReader.HasAttributes )
  {
-    string str1 = !(xmlReader.GetAttribute("TemplateID") == "") ? xmlReader.GetAttribute("TemplateID") : "NULL";
-    string str2 = !(xmlReader.GetAttribute("Name") == "") ? xmlReader.GetAttribute("Name") : "NULL";
+    string str1 = !(xmlReader.GetAttribute("TemplateID") == "") ? xmlReader.GetAttribute("TemplateID") : "null";
+    string str2 = !(xmlReader.GetAttribute("Name") == "") ? xmlReader.GetAttribute("Name") : "null";
             if (str2.IndexOf("'") != -1)
                 str2 = str2.Replace("'", "''");
-            string str3 = !(xmlReader.GetAttribute("Description") == "") ? xmlReader.GetAttribute("Description") : "NULL";
+                str2 = str2.Replace("?", "*");
+            string str3 = !(xmlReader.GetAttribute("Description") == "") ? xmlReader.GetAttribute("Description") : "null";
             if (str3.IndexOf("'") != -1)
                 str3 = str3.Replace("'", "''");
-
-    streamWriter.WriteLine("SET Name = '{0}', Description = '{1}'", str2, str3);
-    streamWriter.WriteLine("WHERE TemplateID = '{0}'", str1);
+            if (str3 != "null")
+            {
+                streamWriter.WriteLine("SET Name = '{0}', Description = '{1}'", str2, str3);
+            }
+            else
+            {
+                streamWriter.WriteLine("SET Name = '{0}', Description = {1}", str2, str3);
+            }
+            streamWriter.WriteLine("WHERE TemplateID = '{0}'", str1);
     streamWriter.WriteLine("UPDATE dbo.Shop_Goods");
 }
 }
@@ -38,7 +43,7 @@ while (xmlReader.Read())
 
 
 new Thread(new ThreadStart(Main)).Start();
+Task.WaitAll();
 Console.WriteLine("Done");
+
 Console.ReadKey();
-
-
